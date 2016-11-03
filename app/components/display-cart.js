1852bec572934b24f371987f0a62c1c09a62fcda
Molly LeCompte and Zachary Matthewstearn <molly@>
2016-11-03 11:27:23 -0700

    add shopping cart service that works.

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-
-  actions: {
-    addToCart(product) {
-      this.get('shoppingCart').add(product);
-    },
-  }
 });
---
+++ tests/unit/helpers/helper-cart-test.js
@@ -0,0 +1,12 @@
+
+import { helperCart } from 'marketplace/helpers/helper-cart';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | helper cart');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = helperCart([42]);
+  assert.ok(result);
+});
+
